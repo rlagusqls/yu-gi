a21503b005286ea15f7e847d6eae392720b826e0
rlagusqls <42687054+rlagusqls@users.noreply.github.com>
2018-08-25 12:39:25 +0900

    Add files via upload

---
+++ drw/sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(300,300);
+  background(255,0,255);
+}
+
+function draw() {
+  fill(0,255,255);
+  noStroke();
+  rect(150,150,150,150)
+  ellipse(150,150,175,175)
+  fill(0,255,255)
+  rect(0,0,150,150)
+  
+}
